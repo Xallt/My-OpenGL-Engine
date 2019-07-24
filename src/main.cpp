046c14b8667437791b5b05f223184483b89ecfc6
Dmitriy Shabat <mitya.shabat@gmail.com>
2019-07-24 19:45:59 +0300

    Did a bit of commenting in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
-#include <glm/gtx/string_cast.hpp>
 
 #include "camera.h"
 #include "window.h"
@@ -13,48 +12,38 @@
 #include "raymarching/rm_scene.h"
 #include "raytracing/rt_scene.h"
 
-// This way it's more convenient
-#define int GLint
-#define float GLfloat
-#define uint GLuint
-
 // Global vars
 double g_time, g_delta;
-Camera *g_camera;
+Camera *cam;
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int modes) {
+  // Close window on ESCAPE
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, GLFW_TRUE);
   }
 }
 
-
-
 void setCallbacks(Window &window) {
   window.setKeyCallback(keyCallback);
 }
 
-void mainCycle(Window &window) {
-  g_camera = new Camera(glm::radians(45.0), window.aspect());
-  g_camera->transform(glm::translate(glm::vec3(0.0, 1.0, 3.0)));
-  CameraController controller(g_camera, &window);
-
-  Object* obj = new RaytracingScene();
-  obj->init();
-
+void mainCycle(Window &window, Camera* cam, Object* obj) {
+  CameraController controller(cam, &window);
   g_time = glfwGetTime();
   g_delta = 0;
   float elapsed = 0;
   int iter = 0;
+
+  obj->init();
   while (!window.shouldClose()) {
     glfwPollEvents();
     controller.update(g_delta);
     obj->update(g_delta);
     
+    // Draw
     glClearColor(0.1484375, 0.203125, 0.52734375, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    obj->render(*g_camera);
-
+    obj->render(*cam);
     window.swapBuffers();
 
     g_delta = glfwGetTime() - g_time;
@@ -62,6 +51,7 @@ void mainCycle(Window &window) {
     elapsed += g_delta;
     ++iter;
   }
+
   cout << "FPS: ~" << (float)iter / elapsed << '\n';
   obj->free();
   glfwTerminate();
@@ -76,7 +66,14 @@ int main () {
   if (!success) {
     return -1;
   }
+
+  Camera* cam = new Camera(glm::radians(45.0), window.aspect());
+  cam->transform(glm::translate(glm::vec3(0.0, 1.0, 3.0)));
+
+  // Setting the main scene
+  Object* obj = new RaytracingScene();
+
   setCallbacks(window);
-  mainCycle(window);
+  mainCycle(window, cam, obj);
   return 0;
 }
